ff6b888a6ed53ade68d135c271b2927a9b212e9e
securisec <ctf@securisec.com>
2018-02-11 05:19:10 -0500

    travis test file

---
+++ .test/test.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import reportng
+
+r = reportng.ReportWriter(report_name='somename', brand='somebrand',
+                          asciinema=True, code_highlight=True, progress_bar=True)
+report = r.report_header()
+content = """
+    # fasfdds
+    # fdsfsfsd
+    # fdsfasdfsd
+    # fdsfsdf
+    # Malmö
+    # 아름다운
+    # 你好"""
+report += r.report_section(title='title', content=content, pre_tag=True,
+                           tag_color='danger', title_color=False)
+report += r.report_image_carousel('a', 'b', a='a', b='b')
+report += r.report_asciinema('https://asciinema.org/a/123683', title='title')
+report += r.report_code_section(title='title', code="""
+    $(document).ready(function() {
+    $('pre code').each(function(i, block) {
+        hljs.highlightBlock(block);
+    });
+    });)""")
+report += r.report_captions('test')
+report += r.report_cards(
+    ('primary', 'a', 'a'), ('danger', 'a', 'a'),
+    section=True, title='title', border_only=True
+)
+report += r.report_footer(message='message', github='a',
+                          linkedin='a', email='a', twitter='a')
+r.report_save(report, 'test.html')
+
+reportng.DownloadAssets.download_assets('', theme='pulse')
