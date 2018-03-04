a82b99baae653c33d7de04eb24d9ed2bea4a08a9
securisec <ctf@securisec.com>
2018-03-03 21:35:16 -0500

    broken print statements

--- test/test.py
+++ test/test.py
@@ -2,13 +2,13 @@
 import reportng
 
 reportng.DownloadAssets(download_path='./', rel_path='./', theme='pulse')
-print 'download_assets'
+print('download_assets')
 
 
 r = reportng.ReportWriter(report_name='somename', brand='somebrand',
                           asciinema=True, code_highlight=True, progress_bar=True)
 report = r.report_header()
-print 'report_header'
+print('report_header')
 content = """
     # fasfdds
     # fdsfsfsd
@@ -19,23 +19,23 @@ content = """
     # 你好""" * 100
 report += r.report_section(title='title', content=content, pre_tag=True,
                            tag_color='red', title_color=False)
-print 'report_section1'
+print('report_section1')
 report += r.report_section(title='title', content=content, pre_tag=True,
                            tag_color='danger', title_color=False, overflow='')
-print 'report_section2'
+print('report_section2')
 report += r.report_image_carousel('a', 'b', a='a', b='b')
-print 'report_image'
+print('report_image')
 report += r.report_asciinema('https://asciinema.org/a/123683', title='title')
-print 'report_asciinema'
+print('report_asciinema')
 report += r.report_code_section(title='title', code="""
     $(document).ready(function() {
     $('pre code').each(function(i, block) {
         hljs.highlightBlock(block);
     });
     });)""")
-print 'report_code'
+print('report_code')
 report += r.report_captions('test')
-print 'report_captions'
+print('report_captions')
 report += r.report_table(
     ('a data', 'b data', 'c data', 'e data', 'f data', 'e data', 'f data', 'e data', 'f data', 'e data', 'f data'),
     ('a data', 'b data', 'c data', 'e data', 'f data', 'e data', 'f data', 'e data', 'f data', 'e data', 'f data'),
@@ -46,12 +46,12 @@ report += r.report_table(
             '4th header', '5th header', '5th header', '5th header', '5th header', '5th header', '5th header',
             '5th header',),
     title='Tables are nonsense')
-print 'report_table'
+print('report_table')
 report += r.report_cards(
     ('primary', 'a', 'a'), ('danger', 'a', 'a'),
     section=True, title='title', border_only=True
 )
-print 'report_cards'
+print('report_cards')
 report += r.report_footer(message='message', github='a',
                           linkedin='a', email='a', twitter='a')
 r.report_save(report, 'test.html')
