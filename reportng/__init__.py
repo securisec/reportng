99ba8381049b9d6ab35682479bb5f1bfad9afce4
securisec <ctf@securisec.com>
2018-03-04 02:14:47 -0500

    0.44 fix in local and download assets

--- reportng/__init__.py
+++ reportng/__init__.py
@@ -9,7 +9,6 @@ import os
 import logging
 from requests import get
 from collections import OrderedDict
-import rnghelpers as rng
 import dominate
 import dominate.tags as tag
 from dominate.util import raw
@@ -20,8 +19,8 @@ if sys.version[0] == '2':
     reload(sys)
     sys.setdefaultencoding('utf-8')
     import rnghelpers as rng
-# elif sys.version[0] == '3':
-#     from . import rnghelpers as rng
+elif sys.version[0] == '3':
+    from . import rnghelpers as rng
 
 __author__ = 'securisec'
 __version__ = '0.44'
